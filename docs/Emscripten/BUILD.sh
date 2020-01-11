82ab92b9b4cf86d21b125f0924708f7b1d5afde8
John D. Haughton <anotherjohnh@gmail.com>
2020-01-11 14:08:50 +0000

    Document update

--- docs/Emscripten/BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ../../Platform/scripts/BUILD_EMSCRIPTEN.sh
-cmake ../.. -DPLT_TARGET=Emscripten
-make
-rm -rf CMake* CPack* Makefile cmake* libPLT.a
--- docs/WebAsm/BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source ../../Platform/scripts/BUILD_WEBASM.sh
-cmake ../.. -DPLT_TARGET=WebAsm
-make
-rm -rf CMake* CPack* Makefile cmake* libPLT.a
